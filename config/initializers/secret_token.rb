ca00737b9410ee45da4bab33b495d67a5af0c53b
Aaron D Williams <aaron@aarondwilliams.org>
2011-07-20 18:31:47 -0700

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vastra::Application.config.secret_token = '758aa25928bde62c7e528a8c62eb175da160a8fe9c3b50a05131081057f1a43622ffba9160629ba1e9bb2c76f108006370f6ed2092d3d31457800d2b4fdfd96e'
